8e1a9ce011d6c9c29e18bb29a0e76d7f460bf113
xingzo <kingsleynyaosi@gmail.com>
2018-03-12 12:17:08 -0700

    created project model and schema

--- server.js
+++ server.js
@@ -49,13 +49,32 @@ app.get('/api', function apiIndex(req, res) {
   res.json({
     woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/xingzo/express-personal-api/README.md", // CHANGE ME
+    baseUrl: "https://dry-sierra-51475.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
       {method: "POST", path: "/api/projects", description: "E.g. Create a new project"} // CHANGE ME
     ]
+  })
+});
+
+app.get('/api/profile', function apiIndex(req, res) {
+  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
+  // It would be seriously overkill to save any of this to your database.
+  // But you should change almost every line of this response.
+  res.json({
+
+    // message: "Welcome to my personal api! Here's what you need to know about me!",
+    // documentationUrl: "https://github.com/xingzo/express-personal-api/README.md",
+
+    name: "Kingsley Nyaosi",
+    githubUserName: "xingzo",
+    githubLink: "https://github.com/xingzo/",
+    githubProfileImage: "https://avatars1.githubusercontent.com/u/34792990?s=400&v=4",
+    personalSiteLink: "http://xingzo.io",
+    currentCity: "Bay Area",
+    skills : [{languages: "javascript, HTML, CSS, jQuery"}, {tools: "bootstrap, github/git"}]
   })
 });
 
